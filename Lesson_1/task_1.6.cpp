49ef52494225f8b91d2afdc741092b62a2716775
Ilya24Moroz <ilya3azot@gmail.com>
2020-02-27 17:32:37 +0200

    Made the task through 3 variables

--- Lesson_1/task_1.6.cpp
+++ Lesson_1/task_1.6.cpp
@@ -11,50 +11,20 @@ int main()
     cin >> b;
     cout << "Enter 3 number: ";
     cin >> c;
-    cout << "1: +" << endl;
-    cout << "2: -" << endl;
-    cout << "3: *" << endl;
-    cout << "4: /" << endl;
-    cout << "Enter action: ";
-    cin >> d;
-    if(d == 1)
+    if(a + b == c || a + c == b || b + c == a)
     {
-        if(a + b == c || a + c == b || b + c == a)
-        {
-            cout << "Yes";
-        }else
-        {
-            cout << "No";
-        }
-    }
-    if(d == 2)
+        cout << "Yes";
+    }else if( a - b == c || a - c == b || b - c == a)
     {
-        if( a - b == c || a - c == b || b - c == a)
-        {
-            cout << "Yes";
-        }else
-        {
-            cout << "No";
-        }
-    }
-    if(d == 3)
+        cout << "Yes";
+    }else if( a * b == c || a * c == b || b * c == a)
     {
-        if( a * b == c || a * c == b || b * c == a)
-        {
-            cout << "Yes";
-        }else
-        {
-            cout << "No";
-        }
-    }
-    if(d == 4)
+        cout << "Yes";
+    }else if(a / b == c || a / c == b || b / c == a)
     {
-        if(a / b == c || a / c == b || b / c == a)
-        {
-            cout << "Yes";
-        }else
-        {
-            cout << "No";
-        }
+        cout << "Yes";
+    }else
+    {
+        cout << "No";
     }
 }
