29ff8bce4d5c04d45046387eaac183c658296da6
Ilya24Moroz <ilya3azot@gmail.com>
2020-02-27 17:25:05 +0200

    Fixed identation in lesson_1 task_1.3.cpp

--- Lesson_1/task_1.3.cpp
+++ Lesson_1/task_1.3.cpp
@@ -13,26 +13,26 @@ int main()
     cin >> c;
     if(a > b and b > c and a > c )
     {
-       cout << a - c;
+        cout << a - c;
     }
-    if(a>c and c>b and a>b )
+    if(a > c and c > b and a > b )
     {
-       cout << a - b;
+        cout << a - b;
     }
     if(b > c and c > a and b > a)
     {
-       cout << b - a;
+        cout << b - a;
     }
     if(b > a and a > c and b > c)
     {
-       cout << b - c;
+        cout << b - c;
     }
     if(c > a and a > b and c > b)
     {
-       cout << c - b;
+        cout << c - b;
     }
     if(c > b and b > a and c > a)
     {
-       cout << c - a;
+        cout << c - a;
     }
 }
