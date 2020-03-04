826db7f38307af2fef96fdec9ad1228588de4340
Ilya24Moroz <ilya3azot@gmail.com>
2020-03-04 20:02:11 +0200

    Changed the tasks algorithm

--- Lesson_2/task_2.2.cpp
+++ Lesson_2/task_2.2.cpp
@@ -5,17 +5,15 @@ using namespace std;
 int main()
 {
     int num;
-    int j = 0;
+    int i = 0;
     cout << "Enter the number: ";
     cin >> num;
-
-
-    for(int i = 0; i <= num; i++){
-      cout << i;
-      while (j < num){
-            cout << ",";
-            break;
-        }
-        j++;
+    while(i<=num){
+        cout << i;
+        i++;
+        cout <<",";
     }
+    cout << i;
+
+
 }
--- Lesson_2/task_2.4.cpp
+++ Lesson_2/task_2.4.cpp
@@ -5,39 +5,34 @@ using namespace std;
 int main()
 {
     int pyramids_size;
-    int stars = 1;
     cout << "Enter the size of the pyramids: ";
     cin >> pyramids_size;
-    for(int pyramid_to_bottom = 0; pyramid_to_bottom < pyramids_size; pyramid_to_bottom++)
-    {
-        for(int counter_stars_up = 0; counter_stars_up < stars; counter_stars_up++){
-            cout << "*";
-        }
+    for (int pyramid_to_bottom = 0; pyramid_to_bottom <= pyramids_size; pyramid_to_bottom++){
+         for (int counter_stars_up = 0; counter_stars_up < pyramid_to_bottom; counter_stars_up++){
+             cout << "*";
+         }
         cout << endl;
-        stars++;
-    }
+       }
     cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-    for(int pyramid_to_top = 0; pyramid_to_top < pyramids_size; pyramid_to_top++)
-    {
-        for(int counter_stars_down = 1; counter_stars_down < stars; counter_stars_down++){
-            cout << "*";
-        }
+    for (int pyramid_to_top = 1; pyramid_to_top <= pyramids_size; pyramid_to_top++){
+         for (int counter_stars_down = pyramid_to_top; counter_stars_down <= pyramids_size; counter_stars_down++){
+             cout << "*";
+         }
         cout << endl;
-        stars--;
-    }
+       }
     cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-    int freespace = 0;
-    stars = pyramids_size;
-    for(int counter_iteration = 0; counter_iteration < pyramids_size; counter_iteration++)
-    {
-        for(int counter_freespace = 0; counter_freespace < freespace; counter_freespace++){
+    for (int counter_iteration = 1; counter_iteration <= pyramids_size; counter_iteration++){
+         for (int counter_freespace = 2; counter_freespace <= counter_iteration; counter_freespace++){
             cout << " ";
-        }
-        freespace++;
-        for(int counter_inverse = 0; counter_inverse < stars; counter_inverse++){
-            cout << "*";
-        }
+         }
+            for (int counter_inverse = counter_iteration; counter_inverse <= pyramids_size; counter_inverse++){
+                cout << "*";
+         }
         cout << endl;
-        stars--;
-    }
+       }
+
+
+
+
 }
+
