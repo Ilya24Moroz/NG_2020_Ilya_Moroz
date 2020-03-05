81442ae7e4b9d59c78c4c986b039ff28f9baa317
Ilya24Moroz <ilya3azot@gmail.com>
2020-03-05 18:42:51 +0200

    Changed the task algorithm

--- Lesson_2/task_2.9.cpp
+++ Lesson_2/task_2.9.cpp
@@ -5,24 +5,19 @@ using namespace std;
 int main()
 {
     int tree_size;
-    int stars = 1;
     cout << "Enter the size of the tree: ";
     cin >> tree_size;
-    int freespace = tree_size;
-
-    for(int tree_iteration = 0; tree_iteration < tree_size; tree_iteration++)
-    {
-        freespace--;
-        for(int freespace_iteration = 0; freespace_iteration < freespace; freespace_iteration++){
+    for (int tree_iteration = 0; tree_iteration < tree_size; tree_iteration++){
+        for (int freespace_iteration = 1; freespace_iteration < tree_size - tree_iteration; freespace_iteration++){
             cout << " ";
         }
-        for(int stars_iteration = 0; stars_iteration < stars; stars_iteration++){
+        for (int freespace_iteration = tree_size - 2 * tree_iteration;
+            freespace_iteration <= tree_size;
+            freespace_iteration++){
             cout << "*";
         }
-        stars += 2;
         cout << endl;
     }
-
     for(int space_iteration = 0; space_iteration < tree_size - 1; space_iteration++){
         cout << " ";
     }
