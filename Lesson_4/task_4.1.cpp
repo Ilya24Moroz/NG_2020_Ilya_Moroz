f1a0a3d99cba90d3e822b77e231ac3e5b065323d
Ilya24Moroz <ilya3azot@gmail.com>
2020-03-19 13:45:03 +0200

    Changed the program algorithm

--- Lesson_4/task_4.1.cpp
+++ Lesson_4/task_4.1.cpp
@@ -5,15 +5,20 @@ using namespace std;
 int main()
 {
     char str[50];
-    int index = 0, counterWords = 0;
+    int index = 0, counterWords = 0, word=0;
     cout << "Enter your string: ";
     cin.getline(str, 100);
 
     while(str[index] != 0){
-        if(str[index] == ' '){
+        if(str[index] >= 'a' && str[index] <= 'z'){
             counterWords++;
         }
+        if(str[index] == ' ' and counterWords > 0){
+            word++;
+            counterWords = 0;
+        }
+
         index++;
     }
-    cout << counterWords + 1 << endl;
+    cout << "Words in your string: " << word + 1 << endl;
 }
