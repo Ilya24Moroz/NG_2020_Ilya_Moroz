d5e697d8ecc6aa80a103cc010bb554d9028a8a26
Ilya24Moroz <ilya3azot@gmail.com>
2020-03-20 18:47:26 +0200

    Made only word counter

--- Lesson_4/task_4.1.cpp
+++ Lesson_4/task_4.1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     char str[50];
-    int index = 0, counterWords = 0, word=0;
+    int index = 0, counterWords = 0, word = 0;
     cout << "Enter your string: ";
     cin.getline(str, 100);
 
@@ -13,12 +13,12 @@ int main()
         if(str[index] >= 'a' && str[index] <= 'z'){
             counterWords++;
         }
-        if(str[index] == ' ' and counterWords > 0){
+        if(!(str[index] >= 'a' and str[index] <= 'z') and counterWords > 0){
             word++;
             counterWords = 0;
         }
-
         index++;
     }
-    cout << "Words in your string: " << word + 1 << endl;
+    word++;
+    cout << "Words in your string: " << word << endl;
 }
