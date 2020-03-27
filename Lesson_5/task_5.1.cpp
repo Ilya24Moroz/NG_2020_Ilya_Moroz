7f79bea22228de1f59dc21fbcb1bdd5864ad0eed
Ilya24Moroz <ilya3azot@gmail.com>
2020-03-27 13:56:55 +0200

    Changed type of function getArr

--- Lesson_5/task_5.1.cpp
+++ Lesson_5/task_5.1.cpp
@@ -2,26 +2,29 @@
 
 using namespace std;
 
-int getArr(int arr[], int sizeArr, int num){
-  for(int counter = 0; counter < sizeArr; counter++){
-      arr[counter] *= num;
+void getArr(int arr[], int sizeArr, int num){
+  for(int counter_multiply = 0; counter_multiply < sizeArr; counter_multiply++){
+      arr[counter_multiply] *= num;
     }
-    for(int counter_output = 0; counter_output < sizeArr; counter_output++){
-        cout << arr[counter_output] << endl;
+  for(int counter_output = 0; counter_output < sizeArr; counter_output++){
+      cout << "Numbers in array: " << arr[counter_output] << endl;
     }
 }
 
 int main()
 {
-  int sizeArr = 0, numArr[sizeArr], number;
+  int sizeArr = 0, number;
   cout << "Enter array size: ";
   cin >> sizeArr;
+  int numArr[sizeArr];
 
   for(int counter_input = 0; counter_input < sizeArr; counter_input++){
       cout << "Enter number to array: ";
       cin >> numArr[counter_input];
     }
-    cout << "Enter number: ";
-    cin >> number;
-    getArr(numArr, sizeArr, number);
+
+  cout << "Enter number on which you want multiply: ";
+  cin >> number;
+
+  getArr(numArr, sizeArr, number);
 }
